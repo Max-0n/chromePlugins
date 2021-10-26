84b1839ef1196148cedad22e1a26ff43590578cf
Max <max0n@me.com>
2021-10-26 15:16:26 +0300

    Add 1 more

--- content.js
+++ content.js
@@ -4,4 +4,26 @@ if (/tabfil/.test(window.location.origin)) {
   } catch(error) {
     console.error('#vid_vpaut_div not found', error);
   }
+
+  try {
+    [...document.getElementsByClassName('adwise-banner')].forEach(item => { item.remove(); });
+  } catch(error) {
+    console.error('.adwise-banner', error);
+  }
+}
+
+// z3.viewlost.life
+if (/viewlost/.test(window.location.origin)) {
+  try {
+    const clearFunc = () => {
+      [...document.body.children].forEach(child => {
+        if (child.className !== 'wrap') child.remove();
+      });
+    };
+    clearFunc();
+
+    setTimeout(() => { clearFunc(); }, 700);
+  } catch(error) {
+    console.error('document.body.children not found', error);
+  }
 }
